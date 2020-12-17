9416ff6777a7455d5c4f4de44f19363631a42b8e
samuelowino <samuelowino43@yahoo.com>
2020-12-17 14:53:07 +0300

    Challenge 5:

--- src/com/coder/challenge5/Challenge5.java
+++ src/com/coder/challenge5/Challenge5.java
@@ -2,10 +2,9 @@ package com.coder.challenge5;
 
 public class Challenge5 {
 
-    // Complete the miniMaxSum function below.
     static void miniMaxSum(int[] arr) {
-        int sum_a = 0;
-        int sum_b = 0;
+        long sum_a = 0;
+        long sum_b = 0;
 
         int[] sortedArr = sort(arr);
 
@@ -26,37 +25,23 @@ public class Challenge5 {
 
     private static int[] sort(int[] arr) {
 
-        System.out.println("INPUT arr [" + arr[0] + "] [" + arr[1] + "] [" + arr[2] + "] [" + arr[3] + "] [" + arr[4] + "]");
-
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 int next = j + 1;
-                System.out.println("\n==j " + j);
-                if (next < arr.length - 1 && arr[j] > arr[next]) {
+                if (next <= arr.length - 1 && arr[j] > arr[next]) {
                     int swapVal = arr[j];
                     arr[j] = arr[next];
                     arr[next] = swapVal;
-
-                    System.out.println("arr [" + arr[0] + "] [" + arr[1] + "] [" + arr[2] + "] [" + arr[3] + "] [" + arr[4] + "]");
-
                 }
             }
         }
-
-        System.out.println();
-        System.out.println();
-        for (int k = 0; k < arr.length; k++) {
-            System.out.println("Sorted Arr| " + arr[k]);
-        }
-
         return arr;
     }
 
 
     public static void main(String[] args) {
-        int[] arr = {7 ,69, 2, 221, 8974};
+        int[] arr = {396285104, 573261094, 759641832, 819230764, 364801279};
         miniMaxSum(arr);
-
     }
 
 }
